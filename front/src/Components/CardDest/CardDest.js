a632ea56a0307ad337ce89ce0a85e6704317427c
Chaouch Nizar <chaouchnizar1@gmail.com>
2023-05-15 06:27:57 +0100

    card dest

--- front/src/Components/CardDest/CardDest.js
+++ front/src/Components/CardDest/CardDest.js
@@ -1,27 +1,19 @@
 import "./CardDest.css";
 import { Link } from "react-router-dom";
-import korbous from "../../Images/korbous/korbous.jpg";
-import { Image } from "react-bootstrap";
+
 
 function CardDest(props) {
-    // const card1 =style.card1'
-    // background: ${(props)=>props.color};
-    // ';
-    let im = props.ima
+    let imageName=props.ima;
+    let korbous=require(`../../Images/${imageName}`);
     return (
         <div class="col-sm-6 col-md-6 col-lg-3 " >
             <Link to="/acceuil" className="mx-4 ">
-                <div className="card">
-                <Image src={korbous}  className=" " fluid/>
-                <img src={korbous} height="70px" width="70px" />
+                <div className="card"  style={{backgroundImage: `url(${korbous})`}}>
                     <h5>{props.title}</h5>
                     <p>{props.subtitle}</p>
-                </div>
-                
-                
-                
+                </div>                
             </Link>
-            {im}
+            {/* {im} */}
         </div>
     );
 }
--- front/src/Pages/Destination/Destination.js
+++ front/src/Pages/Destination/Destination.js
@@ -1,7 +1,6 @@
 import "./Destination.css";
 import CardDest from "../../Components/CardDest/CardDest";
 import Proposer from "../../Components/proposer/proposer";
-import korbous from "../../Images/korbous/korbous.jpg";
 
 function Destination() {
 
@@ -12,27 +11,24 @@ function Destination() {
             id: "d1",
             title: "Korbous",
             subtitle: "Tunisie,Nabeul",
-            image:"../../../public/camping-tent.png",
+            image:"korbous/korbous.jpg",
         },
         {
             id: "d2",
             title: "Oued Zitoun",
             subtitle: "Tunisie,Bizerte",
-            image:"../../../public/camping-tent.png",
-            // image:
+            image:"ouedZen/ouedzen.jpg"
         },
         {
             id: "d3",
             title: "Djebel Ressas",
             subtitle: "Tunisie,Ben Arous,Nabeul",
-            image:"../../../public/camping-tent.png",
-            // image:,
+            image:"djabelRassas/djabelrassas.jpg"
         }, {
             id: "d4",
             title: "Oued Zen",
             subtitle: "Tunisie,Jendouba",
-            image:"../../../public/camping-tent.png",
-            // image:,
+            image:"ouedZen/ouedzen.jpg"
         }
     ]
 
@@ -45,7 +41,7 @@ function Destination() {
                 <div className="row">
                     <div className="col-md-12 text-center">
                         <p id="tn"><Proposer title="La Tunisie" ></Proposer></p>
-                        <p id="dest"><Proposer subtitle="Destinatin De Camping" ></Proposer></p>
+                        <p ><Proposer subtitle="Destinatin De Camping" ></Proposer></p>
                     </div>
                 </div>
             </div>
